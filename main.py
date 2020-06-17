a549057d75e693cc70bf347b295959c2887bd5c4
solarainu <lizhongwen26@163.com>
2020-06-18 04:12:47 +0800

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pytest
 import allure
 
-pytest.main(['-s','-v',r'C:\Users\lizho\Desktop\api_auto_test-master\test_case\test_postman_api.py','--alluredir=./output/report/xml/'])
+pytest.main(['-s','-v',r'./test_case/test_postman_api.py','--alluredir=./output/report/xml/'])
